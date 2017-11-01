705ebefd93e57d80a39ed59be9cf1b88676d0a91
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-01 17:12:49 +0100

    Initial project setup

---
+++ CCNumberValidator/CCNumberValidator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCNumberValidator
+//
+//  Created by Michał Czerniakowski on 01.11.2017.
+//  Copyright © 2017 M-Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
