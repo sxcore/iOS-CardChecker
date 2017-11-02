ce2c3b940a2a7cbaf787ec69e354a8f0b87fb34c
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-02 04:09:12 +0100

    Add Empty View for use in ViewController.

---
+++ Sources/Views/MainView.swift
@@ -0,0 +1,16 @@
+import UIKit
+import SnapKit
+
+class MainView: UIView {
+
+    init(){
+        super.init(frame: .zero)
+    }
+
+
+    // MARK: - Required initializer
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
