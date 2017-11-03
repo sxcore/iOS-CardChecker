28329064c7c9683243d177bdbe2f3cddf1bf200b
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-03 06:54:35 +0100

    Added new types for ValidationView.

--- Sources/Views/Subviews/ValidationIndicatorView.swift
+++ Sources/Views/Subviews/ValidationIndicatorView.swift
@@ -1,6 +1,49 @@
 import SnapKit
 import UIKit
 
+enum ValidationIndicatorViewType {
+    case negative, neutral, positive
+}
+
 class ValidationIndicatorView: UIView {
+
+}
+
+extension ValidationIndicatorViewType {
+
+    var messageText: String {
+        switch self {
+        case .negative:
+            return "Card number is invalid!"
+        case .neutral:
+            return "Waiting for validation..."
+        case .positive:
+            return "Card number is valid."
+        }
+
+    }
+
+    var iconImage: UIImage {
+        switch self {
+        case .negative:
+            return #imageLiteral(resourceName: "result_invalid")
+        case .neutral:
+            return #imageLiteral(resourceName: "waiting_for_result")
+        case .positive:
+            return #imageLiteral(resourceName: "result_valid")
+        }
+
+    }
+
+    var backgroundColor: UIColor {
+        switch self {
+        case .negative:
+            return UIColor.red
+        case .neutral:
+            return UIColor.green
+        case .positive:
+            return UIColor.orange
+        }
+    }
 
 }
