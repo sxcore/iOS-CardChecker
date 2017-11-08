a6925aadd8e41819899ca1776ec00db479d05059
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-08 02:39:16 +0100

    Add Alert to avoid passing nil to textfield.

--- Sources/ViewControllers/MainViewController.swift
+++ Sources/ViewControllers/MainViewController.swift
@@ -46,11 +46,20 @@ class MainViewController: UIViewController, CreditCardNumberDelegate {
     // MARK: - Delegates
 
     func validationButtonClicked(string: String) {
+        if !string.isEmpty {
         guard let stringAsInt = Int(string) else {
             fatalError("Casting string as integer failed")
         }
 
-        fetchCardDetails(number: stringAsInt)
+            fetchCardDetails(number: stringAsInt)
+
+        } else {
+            let alert = UIAlertController(title: "No Input!",
+                                          message: "Input Cannot be empty",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - ControllerProviding
