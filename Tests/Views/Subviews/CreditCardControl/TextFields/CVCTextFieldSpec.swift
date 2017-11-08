be0ea7f60a5b7dbbf8fad7c6ef89429ab6673b5f
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-08 14:13:16 +0100

    Add CVCTextField tests.

---
+++ Tests/Views/Subviews/CreditCardControl/TextFields/CVCTextFieldSpec.swift
@@ -0,0 +1,42 @@
+@testable import CCNumberValidator
+import Nimble
+import Quick
+
+class CVCTextFieldSpec: QuickSpec {
+
+    override func spec() {
+        describe("CVVTextFieldSpec") {
+            var sut: CVCTextField!
+
+            beforeEach {
+                sut = CVCTextField()
+            }
+            afterEach {
+                sut = nil
+            }
+
+            describe("should NOT change characters") {
+                it("in range of 0") {
+                    let singleChar = "X"
+                    let result = sut.textField(sut, shouldChangeCharactersIn: NSRange(location: 0, length: 0), replacementString: singleChar)
+                    expect(result).to(beFalse())
+                }
+
+                it("in range of 3") {
+                    sut.insertText("666")
+                    let result = sut.textField(sut, shouldChangeCharactersIn: NSRange(location: 3, length: 0), replacementString: sut.text!)
+                    expect(result).to(beFalse())
+                }
+            }
+
+            describe("should change characters") {
+                it("in range of 2") {
+                    sut.insertText("51")
+                    let result = sut.textField(sut, shouldChangeCharactersIn: NSRange(location: 2, length: 0), replacementString: sut.text!)
+                    expect(result).to(beTrue())
+                }
+            }
+        }
+    }
+
+}
