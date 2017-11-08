88e65821dd5e5b79b8ab3802981486ee241b5d4f
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-08 04:19:20 +0100

    Refactor Main View

--- Sources/Views/MainView.swift
+++ Sources/Views/MainView.swift
@@ -44,52 +44,6 @@ public class MainView: UIView {
 
     }
 
-    private func configureAutolayout() {
-        backgroundView.snp.makeConstraints { make -> Void in
-            make.edges.equalToSuperview()
-        }
-
-        stackView.snp.makeConstraints { make -> Void in
-            make.left.right.centerY.equalToSuperview()
-        }
-
-        containerView.snp.makeConstraints { make -> Void in
-            make.height.equalTo(50)
-            make.left.equalToSuperview().offset(5)
-            make.right.equalToSuperview().offset(-5)
-        }
-
-        validationPendingView.snp.makeConstraints { make -> Void in
-            make.edges.equalToSuperview()
-        }
-
-        validationPositiveView.snp.makeConstraints { make -> Void in
-            make.edges.equalToSuperview()
-        }
-
-        validationNegativeView.snp.makeConstraints { make -> Void in
-            make.edges.equalToSuperview()
-        }
-
-        creditCardControlView.snp.makeConstraints { make -> Void in
-            make.height.equalTo(40)
-            make.centerX.equalToSuperview()
-        }
-
-        generateButton.snp.makeConstraints { make -> Void in
-            make.left.equalToSuperview().offset(20)
-            make.right.equalToSuperview().offset(-20)
-            make.height.equalTo(60)
-        }
-
-        validateButton.snp.makeConstraints { make -> Void in
-            make.left.equalToSuperview().offset(20)
-            make.right.equalToSuperview().offset(-20)
-            make.height.equalTo(60)
-        }
-
-    }
-
     // MARK: - Gesture recognition
 
     private func setUpGestureRecognition() {
@@ -123,7 +77,7 @@ public class MainView: UIView {
 
     // MARK: - Privates
 
-    private let backgroundView: UIView = BackgroundGradientWithLogo()
+    internal let backgroundView: UIView = BackgroundGradientWithLogo()
     private let tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
 
     // MARK: - Required initializer
