6ec4aceb332c5c2495e53e4268379ec5416edc4e
Michał Czerniakowski <mpczerniakowski@gmail.com>
2017-11-08 06:04:51 +0100

    Block buttons during Call, reorganize folders.

--- Sources/Extensions/MainViewController/MainViewController+Service.swift
+++ Sources/Extensions/MainViewController/MainViewController+Service.swift
@@ -4,6 +4,8 @@ extension MainViewController {
 
     func fetchCardDetails(number: Int) {
         resetToPendingAnimation()
+        setButtons(active: false)
+
         cardDetailsService.fetchCardDetails(creditCard: number, block: { cardDetails in
 
             DispatchQueue.main.async {
@@ -13,14 +15,30 @@ extension MainViewController {
 
                 if cardDetails.valid == "false" {
                     self.invalidAnimation()
-           }}
+                }
+                self.setButtons(active: true)
+            }
         }, error: { error in
             DispatchQueue.main.async {
                 self.invalidAnimation()
+                self.setButtons(active: true)
                 self.mainView.validationNegativeView.messageLabel.text = error.localizedDescription
             }
         })
 
+    }
+
+    private func setButtons(active: Bool) {
+            mainView.generateButton.isUserInteractionEnabled = active
+            mainView.validateButton.isUserInteractionEnabled = active
+        if !active {
+            mainView.validateButton.alpha = 0.3
+            mainView.generateButton.alpha = 0.3
+        }
+        if active {
+            mainView.validateButton.alpha = 1.0
+            mainView.generateButton.alpha = 1.0
+        }
     }
 
 }
